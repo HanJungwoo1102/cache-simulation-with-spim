1606b44b9805039ccf80594b226e02679a8ca0e9
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-05-23 19:46:38 +0900

    feat:fin

--- src/cpu/cache.c
+++ src/cpu/cache.c
@@ -14,7 +14,7 @@ typedef int Data;
 
 typedef struct Block {
 	bool valid;
-	bool dirt;
+	bool dirty;
 	int tag;
 	Data* data;
 	int lengthOfData;
@@ -23,40 +23,70 @@ typedef struct Block {
 typedef struct CacheConfig {
 	int size;
 	int numberOfEntries;
-	int numberOfWays;
+	int numberOfWay;
 	ReplacementPolicy replacementPolicy;
 	WritePolicy writePolicy;
 	int cacheHitTime;
 } CacheConfig;
 
+typedef struct WaySet {
+	Block* blocks;
+	int* recentlyUsed;
+	int firstInIndex;
+} WaySet;
+
+typedef struct Result {
+	int accessCount;
+	int hitCount;
+} Result;
+
 typedef struct Cache {
 	CacheConfig config;
-	Block** entries;
+	WaySet* entries;
+	int tagSize;
+	int indexSize;
+	int blockOffsetSize;
+	Result result;
+	Cache* nextLevelCache;
 } Cache;
 
 typedef struct CacheSystem {
-	Cache L1InstructionCache;
-	Cache L1DataCache;
-	Cache L2Cache;
+	Cache* L1InstructionCache;
+	Cache* L1DataCache;
 	int numberOfLevels;
 	int memoryAccessTime;
 } CacheSystem;
 
 bool isCacheSystemCreated = false;
+int instructionCount = 0;
 CacheSystem cacheSystem;
 
-void createCache(Cache* cache, CacheConfig cacheConfig) {
+int getLog(int src) {
+	int i;
+	int count = 0;
+
+	while(src != 1) {
+		src = src >> 1;
+		count += 1;
+	}
+
+	return count;
+}
+
+Cache* createCache(CacheConfig cacheConfig) {
+	Cache* cache = (Cache *)malloc(sizeof(Cache));
 	cache->config = cacheConfig;
 
-	int lengthOfData = cacheConfig.size / cacheConfig.numberOfEntries / cacheConfig.numberOfWays / sizeof(Data);
+	int lengthOfData = cacheConfig.size / cacheConfig.numberOfEntries / cacheConfig.numberOfWay / sizeof(Data);
 
-	Block** entries = (Block **) malloc(sizeof(Block *) * cacheConfig.numberOfEntries);
+	WaySet* entries = (WaySet *) malloc(sizeof(WaySet) * cacheConfig.numberOfEntries);
 
 	int i, j, k;
-	for (int i = 0; i < cacheConfig.numberOfEntries; i++) {
-		Block* blocks = (Block *) malloc(sizeof(Block) * cacheConfig.numberOfWays);
-		
-		for (j = 0; j < cacheConfig.numberOfWays; j++) {
+	for (i = 0; i < cacheConfig.numberOfEntries; i++) {
+		Block* blocks = (Block *) malloc(sizeof(Block) * cacheConfig.numberOfWay);
+		int* recentlyUsed = (int *) malloc(sizeof(int) * cacheConfig.numberOfWay);
+
+		for (j = 0; j < cacheConfig.numberOfWay; j++) {
 			Data* data = (Data *) malloc(sizeof(Data) * lengthOfData);
 		
 			for (k = 0; k < lengthOfData; k++) {
@@ -66,41 +96,68 @@ void createCache(Cache* cache, CacheConfig cacheConfig) {
 			blocks[j].tag = 0;
 			blocks[j].data = data;
 			blocks[j].lengthOfData = lengthOfData;
+			blocks[j].valid = false;
+			blocks[j].dirty = false;
 		}
 
-		entries[i] = blocks;
+		for (j = 0; j < cacheConfig.numberOfWay; j++) {
+			recentlyUsed[j] = 0;
+		}
+
+		entries[i].blocks = blocks;
+		entries[i].recentlyUsed = recentlyUsed;
+		entries[i].firstInIndex = 0;
 	}
 
+	cache->blockOffsetSize = getLog(lengthOfData);
+	cache->indexSize = getLog(cacheConfig.numberOfEntries);
+	cache->result.accessCount = 0;
+	cache->result.hitCount = 0;
 	cache->entries = entries;
+	cache->nextLevelCache = NULL;
+
+	return cache;
 }
 
 void createCacheSystem() {
+	// FILE* file = fopen("../CPU/cache.config", "r");
+	// char buffer[100];
+	// fgets(buffer, sizeof(buffer), file);    // hello.txt에서 문자열을 읽음
+
+	// printf("%s\n", buffer);
+	// fclose(file);
+
 	int numberOfLevels = 2;
 	int memoryAccessTime = 400;
-	CacheConfig l1CacheConfig = { 16, 2, 1, LRU, WT, 1 };
+	CacheConfig l1CacheConfig = { 16, 2, 1, LRU, WT, 0 };
 	CacheConfig l2CacheConfig = { 64, 4, 2, FIFO, WB, 10 };
 
-	cacheSystem.numberOfLevels = numberOfLevels;
-	cacheSystem.memoryAccessTime = memoryAccessTime;
+	Cache* L1DataCache = NULL;
+	Cache* L1InstructionCache = NULL;
+	Cache* L2Cache = NULL;
 
-	createCache(&(cacheSystem.L1DataCache), l1CacheConfig);
-	createCache(&(cacheSystem.L1InstructionCache), l1CacheConfig);
+	cacheSystem.L1DataCache = createCache(l1CacheConfig);
+	cacheSystem.L1InstructionCache = createCache(l1CacheConfig);
 
 	if (numberOfLevels == 2) {
-		createCache(&(cacheSystem.L2Cache), l2CacheConfig);
+		L2Cache = createCache(l2CacheConfig);
 	}
+	cacheSystem.numberOfLevels = numberOfLevels;
+	cacheSystem.memoryAccessTime = memoryAccessTime;
+	cacheSystem.L1DataCache->nextLevelCache = L2Cache;
+	cacheSystem.L1InstructionCache->nextLevelCache = L2Cache;
 
 	isCacheSystemCreated = true;
 }
 
-void printCacheSystem() {
+void printCache(Cache* cache) {
 	int i, j, k;
-	printf("Level 1 Instruction Cache\n");
-	for (i = 0; i < cacheSystem.L1InstructionCache.config.numberOfEntries; i++) {
-		Block* entry = cacheSystem.L1InstructionCache.entries[i];
-		for (j = 0; j < cacheSystem.L1InstructionCache.config.numberOfWays; j++) {
-			Block block = entry[j];
-			printf(" [0x%x] ", block.tag);
+	for (i = 0; i < cache->config.numberOfEntries; i++) {
+		WaySet waySet = cache->entries[i];
+		printf(" [%d] ||", i);
+		for (j = 0; j < cache->config.numberOfWay; j++) {
+			Block block = waySet.blocks[j];
+			printf(" (%d) (%d) (0x%x) ", block.valid, block.dirty, block.tag);
 			for (k = 0; k < block.lengthOfData; k++) {
 				printf(" %d ", block.data[k]);
 			}
@@ -108,71 +165,207 @@ void printCacheSystem() {
 		}
 		printf("\n");
 	}
+}
+
+void printCacheSystem() {
+	printf("\nLevel 1 Instruction Cache\n");
+	printCache(cacheSystem.L1InstructionCache);
 	
 	printf("Level 1 Data Cache\n");
-	for (i = 0; i < cacheSystem.L1DataCache.config.numberOfEntries; i++) {
-		Block* entry = cacheSystem.L1DataCache.entries[i];
-		for (j = 0; j < cacheSystem.L1DataCache.config.numberOfWays; j++) {
-			Block block = entry[j];
-			printf(" [0x%x] ", block.tag);
-			for (k = 0; k < block.lengthOfData; k++) {
-				printf(" %d ", block.data[k]);
-			}
-			printf("|");
+	printCache(cacheSystem.L1DataCache);
+
+	if (cacheSystem.numberOfLevels == 2) {
+		printf("Level 2 Data Cache\n");
+		printCache(cacheSystem.L1DataCache->nextLevelCache);
+	} 
+
+	printf("-------------------------------\n");
+}
+
+int getTag(Cache cache, unsigned int addr) {
+	return addr >> (2 + cache.blockOffsetSize + cache.indexSize);
+}
+
+int getIndex(Cache cache, unsigned int addr) {
+	return (addr >> (2 + cache.blockOffsetSize)) & (cache.config.numberOfEntries - 1);
+}
+
+int change(Cache* cache, unsigned int addr) {
+	int i;
+	int stallCycles = cache->config.cacheHitTime;
+	int index = getIndex(*cache, addr);
+	int tag = getTag(*cache, addr);
+
+	if (cache->config.writePolicy == WT) {
+		if (cache->nextLevelCache == NULL) {
+			stallCycles += cacheSystem.memoryAccessTime;
+		} else {
+			stallCycles += change(cache->nextLevelCache, addr);
 		}
-		printf("\n");
+		printf(" (change WT) ");
+	} else if (cache->config.writePolicy == WB) {
+		for (i = 0; i < cache->config.numberOfWay; i++) {
+			if (cache->entries[index].blocks[i].tag == tag) {
+				cache->entries[index].blocks[i].dirty = true;
+				printf(" (change WB) " );
+			}
+		}
 	}
 
-	printf("Level 2 Data Cache\n");
-	for (i = 0; i < cacheSystem.L2Cache.config.numberOfEntries; i++) {
-		Block* entry = cacheSystem.L2Cache.entries[i];
-		for (j = 0; j < cacheSystem.L2Cache.config.numberOfWays; j++) {
-			Block block = entry[j];
-			printf(" [0x%x] ", block.tag);
-			for (k = 0; k < block.lengthOfData; k++) {
-				printf(" %d ", block.data[k]);
-			}
-			printf("|");
-		}
-		printf("\n");
+	return stallCycles;
+}
+
+int insert(Cache* cache, int index, int tag, unsigned int addr) {
+	int blockPlace, i;
+	int stallCycles = 0;
+
+	WaySet waySet = cache->entries[index];
+
+	for (blockPlace = 0; blockPlace < cache->config.numberOfWay; blockPlace++) {
+		if (waySet.blocks[blockPlace].valid == false) break;
 	}
-	printf("-------------------------------\n");
+
+	if (blockPlace == cache->config.numberOfWay) {
+		if (cache->config.replacementPolicy == LRU) {
+			int leastUsed = waySet.recentlyUsed[0];
+			blockPlace = 0;
+			for (i = 0; i < cache->config.numberOfWay; i++) {
+				if (waySet.recentlyUsed[i] < leastUsed) {
+					blockPlace = i;
+					leastUsed = waySet.recentlyUsed[i];
+				}
+			}
+			printf(" (replace LRU %dth block) ", blockPlace);
+		} else if (cache->config.replacementPolicy == FIFO) {
+			blockPlace = waySet.firstInIndex;
+			cache->entries[index].firstInIndex = (waySet.firstInIndex == cache->config.numberOfWay)
+				? 0
+				: waySet.firstInIndex + 1;
+			printf(" (replace FIFO %dth block) ", blockPlace);
+		}
+	}
+
+	if (waySet.blocks[blockPlace].dirty) {
+		if (cache->nextLevelCache == NULL) {
+			stallCycles += cacheSystem.memoryAccessTime;
+		} else {
+			stallCycles += change(cache->nextLevelCache, addr);
+		}
+	}
+
+	cache->entries[index].blocks[blockPlace].tag = tag;
+	cache->entries[index].blocks[blockPlace].valid = true;
+	cache->entries[index].blocks[blockPlace].dirty = false;
+	printf(" (insert %x, %x, %d, %d) ", index, tag, blockPlace, instructionCount);
+	return stallCycles;
+}
+
+bool access(Cache* cache, int index, int tag) {
+	int i;
+	int stallCycle = cache->config.cacheHitTime;
+
+	cache->result.accessCount += 1;
+	for (i = 0; i < cache->config.numberOfWay; i++) {
+		Block block = cache->entries[index].blocks[i];
+
+		if (block.tag == tag) {
+			cache->result.hitCount += 1;
+			cache->entries[index].recentlyUsed[i] = cache->result.accessCount;
+			printf(" (hit!!) ");
+			return true;
+		}
+	}
+	printf(" (missㅠㅠ) ");
+	return false;
+}
+
+int loadCache(Cache* cache, unsigned int addr) {
+	int stallCycle = cache->config.cacheHitTime;
+	int index = getIndex(*cache, addr);
+	int tag = getTag(*cache, addr);
+
+	if (!access(cache, index, tag)) {
+		if (cache->nextLevelCache == NULL) {
+			stallCycle += cacheSystem.memoryAccessTime;
+		} else {
+			stallCycle += loadCache(cache->nextLevelCache, addr);
+		}
+		stallCycle += insert(cache, index, tag, addr);
+	}
+
+	return stallCycle;
+}
+
+int loadInstCache(unsigned int addr) {
+	return loadCache(cacheSystem.L1InstructionCache, addr);
+}
+
+int loadDataCache(unsigned int addr) {
+	return loadCache(cacheSystem.L1DataCache, addr);
+}
+
+int storeDataCache(unsigned int addr) {
+	return change(cacheSystem.L1DataCache, addr);
 }
 
 int data_load (unsigned int addr) {
 	/* You have to implement your own data_load function here! */
+	int stallCycles = 0;
+	instructionCount += 1;
+	if (instructionCount < 8) return 0;
+
 	if (!isCacheSystemCreated) {
 		createCacheSystem();
 	}
-	printf("LOAD DATA - 0x%x\n", addr);
+
+	stallCycles += loadDataCache(addr);
+
+	printf("\nLOAD DATA - 0x%x (%d)\n", addr, stallCycles);
 	printCacheSystem();
 
-
-	return 0;		// Return value: stall cycles due to L1 cache miss
+	return stallCycles;
+	// Return value: stall cycles due to L1 cache miss
 	///////////////////////////////////////////////////////////
 }
 
 int instruction_load (unsigned int addr) {
 	/* You have to implement your own instruction_load function here! */
+	int stallCycles = 0;
+	instructionCount += 1;
+	if (instructionCount < 8) return 0;
+	
 	if (!isCacheSystemCreated) {
 		createCacheSystem();
 	}
-	printf("LOAD INST - 0x%x\n", addr);
+
+	stallCycles += loadInstCache(addr);
+
+	printf("\nLOAD INST - 0x%x (%d)\n", addr, stallCycles);
 	printCacheSystem();
 
-	return 0;		// Return value: stall cycles due to L1 cache miss 
+	return stallCycles;
+	// Return value: stall cycles due to L1 cache miss 
 	///////////////////////////////////////////////////////////
 }
 
 int data_store(unsigned int addr) {
 	/* You have to implement your own data_store function here! */
+	int stallCycles = 0;
+	instructionCount += 1;
+	if (instructionCount < 8) return 0;
+
 	if (!isCacheSystemCreated) {
 		createCacheSystem();
 	}
-	printf("STORE DATA - 0x%x\n", addr);
+
+	stallCycles += loadDataCache(addr);
+	stallCycles += storeDataCache(addr);
+	
+	printf("\nSTORE DATA - 0x%x (%d)\n", addr, stallCycles);
 	printCacheSystem();
 
-	return 0;		// Return value: stall cycles due to L1 cache miss
+	return stallCycles;
+	// Return value: stall cycles due to L1 cache miss
 	///////////////////////////////////////////////////////////
 }
 
@@ -198,10 +391,51 @@ void print_cache_result(int n_cycles) {
 	*/
 
 	/* You have to implement your own print_cache_result function here! */
+	float l2HitRate;
+	instructionCount += 1;
+
 	if (!isCacheSystemCreated) {
 		createCacheSystem();
 	}
+	float l1iAccessCount = cacheSystem.L1InstructionCache->result.accessCount;
+	float l1iHitCount = cacheSystem.L1InstructionCache->result.hitCount;
+	float l1iHitRate = l1iHitCount / l1iAccessCount;
 
+	float l1dAccessCount = cacheSystem.L1DataCache->result.accessCount;
+	float l1dHitCount = cacheSystem.L1DataCache->result.hitCount;
+	float l1dHitRate = l1dHitCount / l1dAccessCount;
+
+	if (cacheSystem.numberOfLevels == 2) {
+		float l2AccessCount = cacheSystem.L1DataCache->nextLevelCache->result.accessCount;
+		float l2HitCount = cacheSystem.L1DataCache->nextLevelCache->result.hitCount;
+		l2HitRate = l2HitCount / l2AccessCount;
+	}
+
+	float totalAccessCount = cacheSystem.L1InstructionCache->result.accessCount + cacheSystem.L1DataCache->result.accessCount;
+	float totalHitCount = cacheSystem.L1InstructionCache->result.hitCount + cacheSystem.L1DataCache->result.hitCount;
+	if (cacheSystem.numberOfLevels == 2) {
+		totalHitCount += cacheSystem.L1DataCache->nextLevelCache->result.hitCount;
+	}
+	float totalHitRate = totalHitCount / totalAccessCount;
+
+	printf("\n");
+	printf("Level 1 Cache\n");
+	printf("Hit Count of l1i-cache: %d\n", cacheSystem.L1InstructionCache->result.hitCount);
+	printf("Miss Count of l1i-cache: %d\n", cacheSystem.L1InstructionCache->result.accessCount - cacheSystem.L1InstructionCache->result.hitCount);
+	printf("Hit Ratio of l1i-cache: %f\n", l1iHitRate);
+	printf("\n");
+	printf("Hit Count of l1d-cache: %d\n", cacheSystem.L1DataCache->result.hitCount);
+	printf("Miss Count of l1d-cache: %d\n", cacheSystem.L1DataCache->result.accessCount - cacheSystem.L1DataCache->result.hitCount);
+	printf("Hit Ratio of l1d-cache: %f\n", l1dHitRate);
+	printf("\n");
+	printf("Level 2 Cache\n");
+	if (cacheSystem.numberOfLevels == 2) {
+		printf("Hit Count: %d\n", cacheSystem.L1DataCache->nextLevelCache->result.hitCount);
+		printf("Miss Count: %d\n", cacheSystem.L1DataCache->nextLevelCache->result.accessCount - cacheSystem.L1DataCache->nextLevelCache->result.hitCount);
+		printf("Hit Ratio: %f\n", l2HitRate);
+	}
+	printf("\n");
+	printf("Total Hit Ratio: %f\n", totalHitRate);
 
 	//////////////////////////////////////////////////////////////////////
 }
